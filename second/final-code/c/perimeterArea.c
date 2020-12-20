6c5fe4fab8c97bea9cc377cdc5182d527627048b
arubenruben <almeidaruben35@gmail.com>
2020-12-20 21:54:05 +0000

    Aula 2 final codigo

--- second/final-code/c/perimeterArea.c
+++ second/final-code/c/perimeterArea.c
@@ -1,17 +1,26 @@
-#include <stdio.h>
-
 //Este programa calcula o perimetro e a area de um circulo
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
 int main()
 {
     double raio = 0;
-    const double PI = 3.1415;
-
+    double pi = 3.1415;
+    char letra = 'a';
+    bool booleano = true;
+    int inteiro = 4;
+    inteiro = "Pedro";
     printf("Escolher o raio \n");
     scanf("%lf", &raio);
 
-    printf("O perimetro deste circulo e:%lf\n\n", 2 * PI * raio);
+    printf("O perimetro deste circulo e:%lf\n\n", 2 * M_PI * raio);
 
-    printf("A area deste circulo e:%lf\n\n", PI * raio * raio);
+    printf("A area deste circulo e:%lf\n\n", M_PI * pow(raio, 2));
 
+    if (/* condition */)
+    {
+        //codigo executado quando verdadeiro
+    }
     return 0;
 }
--- second/final-code/python/operations.py
+++ second/final-code/python/operations.py
@@ -1,17 +1,18 @@
-# Este programa vai executar as operações elementares numéricas e booleanas
+# Este programa vai executar as operações elementares numéricas
 
 import isNumeric
 
+texto=4
+
 numberOne = input("Inserir o primeiro numero\n")
-numberTwo = input("Inserir o segundo numero. Para nada escrever     no    \n")
+numberTwo = input("Inserir o segundo numero.\n")
 
 
-print(numberOne, numberTwo)
-
-if isNumeric.isNumeric(numberTwo):
+if isNumeric.isNumeric(numberOne) and isNumeric.isNumeric(numberTwo):
     print("Soma:", int(numberOne)+int(numberTwo), "\n")
     print("SubTracao:", int(numberOne)-int(numberTwo), "\n")
     print("Multiplicacao:", int(numberOne)*int(numberTwo), "\n")
     print("Divisao:", int(numberOne)/int(numberTwo), "\n")
     print("Resto:", int(numberOne) % int(numberTwo), "\n")
 
+if not(isNumeric.isNumeric(numberOne) and isNumeric.isNumeric(numberTwo)):
