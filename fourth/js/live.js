c141aa373da040b38f38985cfa5e6de723ec812b
arubenruben <almeidaruben35@gmail.com>
2021-01-10 17:31:18 +0000

    Fourth Class

---
+++ fourth/js/live.js
@@ -0,0 +1,46 @@
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nome = 'Francisca'
+let apelido = 'Baptista'
+
+//let nomeCompleto = nome.concat(apelido);
+let nomeCompleto = nome + apelido
+
+let numero = "77"
+let numero2 = "88"
+numero + numero2
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#result-inline').innerHTML = "O Resultado do fatorial de " + copy + " é " + resultado
+
+
+}
+
+
+
